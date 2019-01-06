1a4c3e7ad25bf05b64afec51a9699124394e5bde
Yoo-Nah <ypark2270@gmail.com>
2019-01-06 13:09:34 -0500

    Add Navbar component

--- src/pages/Home/component.js
+++ src/pages/Home/component.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from '../../components/Header';
+import Navbar from '../../components/Navbar';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Navbar />
     </div>
   );
 };
